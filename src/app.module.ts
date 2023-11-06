1b1b7329d87ea7967b31650b34fa9cc8bc77128e
Jairo Koning <jairo.koning@gmail.com>
2023-11-06 20:00:30 -0300

    chore: add movies resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      database: 'app',
+      username: 'postgres',
+      password: '123456',
+      entities: [],
+      synchronize: true,
+    }),
+    MoviesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
