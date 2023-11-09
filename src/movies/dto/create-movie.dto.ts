287010874186d45eb6fc4eac3abdaadb3c92d19d
Jairo Koning <jairo.koning@gmail.com>
2023-11-09 20:58:32 -0300

    feat(create movie): add validation for required props

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,6 +1,16 @@
+import { IsInt, IsNotEmpty } from 'class-validator';
+
 export class CreateMovieDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
+
+  @IsNotEmpty()
   genre: string;
+
+  @IsNotEmpty()
+  @IsInt()
   release: number;
 }
