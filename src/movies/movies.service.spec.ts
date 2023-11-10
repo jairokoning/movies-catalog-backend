34a1b486a7d93ba9584b25769ef270a86e56356e
Jairo Koning <jairo.koning@gmail.com>
2023-11-10 09:16:22 -0300

    refactor(movies.service.spec): add more clean code

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -6,6 +6,38 @@ import { Repository } from 'typeorm';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { ConflictException } from '@nestjs/common/exceptions';
 
+const movieEntityList: Movie[] = [
+  new Movie({
+    title: 'Mr Beans Holiday',
+    description:
+      'The hapless Mr. Bean takes a vacation on the French Riviera, where he becomes ensnared in an accidental kidnapping and a case of mistaken identity.',
+    genre: 'Comedies',
+    release: 2022,
+  }),
+  new Movie({
+    title: 'Saving Private Ryan',
+    description:
+      'After braving D-Day, Capt. John Miller leads a band of soldiers behind enemy lines to find a paratrooper whose three brothers have been killed in action.',
+    genre: 'Drama',
+    release: 1998,
+  }),
+  new Movie({
+    title: 'Race to the Summit',
+    description:
+      'Fearless alpine climbers Ueli Steck and Dani Arnold enter into a death-defying rivalry to set speed records on the Swiss Alps great north faces.',
+    genre: 'Documentaries',
+    release: 2023,
+  }),
+];
+
+const updatedMovieEntityItem = new Movie({
+  title: 'Saving Private Ryan',
+  description:
+    'After braving D-Day, Capt. John Miller leads a band of soldiers behind enemy lines to find a paratrooper whose three brothers have been killed in action.',
+  genre: 'War | Drama',
+  release: 1998,
+});
+
 describe('MoviesService', () => {
   let moviesService: MoviesService;
   let movieRepository: Repository<Movie>;
@@ -18,9 +50,17 @@ describe('MoviesService', () => {
           provide: getRepositoryToken(Movie),
           useValue: {
             create: jest.fn(),
-            save: jest.fn(),
-            findOne: jest.fn(),
-            findOneOrFail: jest.fn(),
+            // save: jest.fn(),
+            // find: jest.fn(),
+            // findOne: jest.fn(),
+            // findOneOrFail: jest.fn(),
+            //create: jest.fn().mockReturnValue(movieEntityList[0]),
+            save: jest.fn().mockResolvedValue(movieEntityList[0]),
+            find: jest.fn().mockResolvedValue(movieEntityList),
+            findOne: jest.fn().mockResolvedValue(movieEntityList[0]),
+            //findOneOrFail: jest.fn().mockResolvedValue(movieEntityList[0]),
+            merge: jest.fn().mockReturnValue(updatedMovieEntityItem),
+            softDelete: jest.fn().mockReturnValue(undefined),
           },
         },
       ],
@@ -46,23 +86,22 @@ describe('MoviesService', () => {
     const movieEntityMock = { ...data } as Movie;
     jest.spyOn(movieRepository, 'create').mockReturnValueOnce(movieEntityMock);
     jest.spyOn(movieRepository, 'save').mockResolvedValueOnce(movieEntityMock);
+    jest.spyOn(movieRepository, 'findOne').mockResolvedValueOnce(undefined);
     const output = await moviesService.create(data);
-
     expect(output).toBeDefined();
+    expect(output).toEqual(data);
     expect(movieRepository.create).toHaveBeenCalledTimes(1);
     expect(movieRepository.save).toHaveBeenCalledTimes(1);
   });
 
   it('should throw new error if movie already exists', async () => {
     const data: CreateMovieDto = {
-      title: 'Saving Private Ryan',
+      title: 'Mr Beans Holiday',
       description:
-        'After braving D-Day, Capt. John Miller leads a band of soldiers behind enemy lines to find a paratrooper whose three brothers have been killed in action.',
-      genre: 'Drama',
-      release: 1998,
+        'The hapless Mr. Bean takes a vacation on the French Riviera, where he becomes ensnared in an accidental kidnapping and a case of mistaken identity.',
+      genre: 'Comedies',
+      release: 2022,
     };
-    const movieEntityMock = { ...data } as unknown as Promise<Movie>;
-    jest.spyOn(movieRepository, 'findOne').mockReturnValueOnce(movieEntityMock);
     await expect(moviesService.create(data)).rejects.toThrow(
       new ConflictException('Movie already exists'),
     );
